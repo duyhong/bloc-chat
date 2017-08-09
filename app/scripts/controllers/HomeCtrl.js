38fddde72d7196ece817c30c7227dfa391bef399
duyhong <duyhong@berkeley.edu>
2017-08-09 14:14:29 -0700

    Modal controller does not work

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,36 @@
 (function() {
-    function HomeCtrl(Room) {
+    function HomeCtrl(Room, $scope, $uibModal) {
         this.rooms = Room.all;
+        $scope.open = function(size, template) {
+            $uibModal.open({
+              animation: true,
+              templateUrl: template || '/templates/addRoom.html',
+              controller: 'AddCtrl',
+              backdrop: 'static'
+            });
+        };
     }
 
+    //main controller
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$scope', '$uibModal', HomeCtrl]);
+    
+    //our modal controller
+    angular
+        .module('blocChat')
+        .controller('AddCtrl', function($scope, $uibModalInstance) {
+            // we'll use this in our `ng-model` for the text input in the view
+            $scope.roomName = '';
+            
+            $scope.ok = function() {
+                Room.add($scope.roomName);
+                alert($scope.roomName + ' is created');
+                $uibModalInstance.close($scope.roomName);
+            };
+        
+            $scope.cancel = function() {
+                $uibModalInstance.dismiss('cancel');
+            }
+        });
 })();
