a3d5e51381f7d71b9fe3b4eca511b834014bf7c2
duyhong <duyhong@berkeley.edu>
2017-08-09 17:43:29 -0700

    Revised 1st time, still does not work

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,21 +1,4 @@
 (function() {
-    function HomeCtrl(Room, $scope, $uibModal) {
-        this.rooms = Room.all;
-        $scope.open = function() {
-            $uibModal.open({
-              animation: true,
-              templateUrl: 'addRoom.html',
-              controller: 'ModalCtrl as modal',
-              backdrop: 'static'
-            });
-        };
-    }
-
-    //main controller
-    angular
-        .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$scope', '$uibModal', HomeCtrl]);
-    
     angular
         .module('blocChat')
         .controller('ModalCtrl', ['Room', '$uibModalInstance', function(Room, $uibModalInstance) {
