dc619833274d810dab33a1f2f2beeace7305bbb9
duyhong <duyhong@berkeley.edu>
2017-08-18 17:30:31 -0700

    blocChat-checkpoint6: could send a message but couldn't show the new message

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -3,13 +3,13 @@
     var Message = {};
     var ref = firebase.database().ref().child("messages");
     //console.log(ref)
-    //var messages = $firebaseArray(ref);
+    var messages = $firebaseArray(ref);
     //console.log(messages);
     
     //Message.all = messages;
       
     Message.getByRoomId = function(roomId) {
-        //console.log(roomId);
+        console.log(roomId);
         
         // Filter the messages by their room ID.
         return $firebaseArray(ref.orderByChild('roomID').equalTo(roomId));
@@ -21,6 +21,9 @@
     };
       
     Message.send = function(newMessage, activeRoom) {
+        console.log(newMessage);
+        console.log(activeRoom);
+        
         var username = $cookies.get('blocChatCurrentUser');
         var date = new Date();
         var time = $filter('date')(new Date(), 'hh:mm:ss a');
